6fa948e717908f8e9ccba869a7f02bc7352efde7
jimbobding <jimmygrahamdavid@gmail.com>
2021-08-24 22:46:37 +0000

    added data

--- shame_cocktail_database/home/models.py
+++ shame_cocktail_database/home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Drinks(models.Model):
+    name = models.CharField(max_length=100)
+    ingredients = models.TextField()
+    method = models.TextField()
+    glassware = models.CharField(max_length=100)
+    garnish = models.CharField(max_length=100)
+    notes = models.CharField(max_length=100)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
--- shame_cocktail_database/home/views.py
+++ shame_cocktail_database/home/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Drinks
 
 
 def home(request):
-    return render(request, "home/index.html")
+
+    context = {
+        'drinks': Drinks.objects.all()
+    }
+    return render(request, "home/index.html", context)
 
 # Create your views here.
