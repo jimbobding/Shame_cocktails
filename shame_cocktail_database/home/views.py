10e2ab4c979e2bc8a8fc8a907ca9579f37e6f9d6
jimbobding <jimmygrahamdavid@gmail.com>
2021-08-24 15:45:51 +0000

    initial commit

---
+++ shame_cocktail_database/home/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
+def home(request):
+    return render(request, "home/index.html")
+
+# Create your views here.
